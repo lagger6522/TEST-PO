76108256bdd491c32f1007ce3917b65ef57c37a3
Ballamut <shulyak.1975@bk.ru>
2022-11-13 13:03:05 +0300

    sad

---
+++ LABA8/aircompany/Net/Aircompany/Models/ExperimentalTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Aircompany.Models
+{
+    public enum ExperimentalTypes
+    {
+        LIFTING_BODY,
+        HYPERSONIC,
+        HIGH_ALTITUDE,
+        VERTICAL_TAKE_OFF_LANDING
+        //VTOL
+    }
+}
---
+++ LABA8/aircompany/Net/Aircompany/Planes/PassengerPlane.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aircompany.Planes
+{
+    public class PassengerPlane : Plane
+    {
+        public readonly int _passengersCapacity; //readonly
+
+        public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            _passengersCapacity = passengersCapacity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PassengerPlane plane && //as =>is
+                   base.Equals(obj) &&
+                   _passengersCapacity == plane._passengersCapacity;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 751774561;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + _passengersCapacity.GetHashCode();
+            return hashCode;
+        }
+
+        public int PassengersCapacityIs()
+        {
+            return _passengersCapacity;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}", ", passengersCapacity=" + _passengersCapacity + '}');
+        }
+
+    }
+}
